e0120dd646c323afc0d3adbc1e981bb7f730315e
farhinmomin <farhinmomin@168.28.56.228>
2016-04-14 14:53:12 -0400

    Merge origin/master
    Conflicts:
    	src/shproject/systemState.fxml

--- src/shproject/FXMLDocumentController.java
+++ src/shproject/FXMLDocumentController.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -20,6 +21,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
@@ -41,9 +43,12 @@ public class FXMLDocumentController implements Initializable {
     
     private SafeHome sh;
     
+  
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
+       
     }
     
     @Override
@@ -51,10 +56,11 @@ public class FXMLDocumentController implements Initializable {
         // TODO
         sh = Context.getInstance().getSafeHome();
     }    
-
+ 
 
     @FXML
     private void handleSubmit(ActionEvent event) throws IOException {
+        
         if(sh.login(username.getText(),password.getText())){
             Parent welcomeScreenParent = FXMLLoader.load(getClass().getResource("Welcome.fxml"));
             Scene welcomScreenScene = new Scene(welcomeScreenParent);
@@ -66,23 +72,6 @@ public class FXMLDocumentController implements Initializable {
         else{
             hiddenlabel.setText("Invalid Username or Password");
         }
-    }
-
-    @FXML
-    private void handleSubmit(KeyEvent event) throws IOException {
-        if(event.getCode().toString().equals("ENTER")) {
-            if(sh.login(username.getText(),password.getText())){
-            Parent welcomeScreenParent = FXMLLoader.load(getClass().getResource("Welcome.fxml"));
-            Scene welcomScreenScene = new Scene(welcomeScreenParent);
-            Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            appStage.hide();
-            appStage.setScene(welcomScreenScene);
-            appStage.show();
-        }
-        else{
-            hiddenlabel.setText("Invalid Username or Password");
-        }
-        }
-    }
+}
     
 }
