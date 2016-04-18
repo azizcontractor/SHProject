666c9100196cfeeb6ae86d790520d81377598dea
azizcontractor <azizcontractor786@gmail.com>
2016-04-18 11:28:51 -0400

    Merge origin/master
    Conflicts:
    	src/shproject/HomeStatePageController.java

--- src/objects/Camera.java
+++ src/objects/Camera.java
@@ -5,16 +5,98 @@
  */
 package objects;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import javafx.scene.image.Image;
+import utils.OracleConnection;
+
 /**
  *
- * @author Meera
+ * @author 
  */
+
 public class Camera {
+    
+    private Image img;
+    private Connection conn;
+    private String locName;
+    private String id;
+    private double panAngle;
+    private double zoom;
+    
     public Camera() {
-        super();
+        
     }
     public void doSomething(){
         return;
     }
+    
+    public ArrayList<Camera> getCameras(){
+        conn = OracleConnection.getConnection();
+        ArrayList<Camera> cameras = new ArrayList<Camera>();
+        try{
+            String sql = "select * from camera";
+            Statement s = conn.createStatement();
+            ResultSet r = s.executeQuery(sql);
+            while (r.next()){
+                Camera c = new Camera();
+                c.setId(r.getString(1));
+                c.setLocName(r.getString(2));
+                c.setPanAngle(r.getDouble(6));
+                c.setZoom(r.getDouble(7));
+                cameras.add(c);
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }finally{
+            OracleConnection.closeConnection();
+        }
+        return cameras;
+    }
+
+    public Image getImg() {
+        return img;
+    }
+
+    public void setImg(Image img) {
+        this.img = img;
+    }
+
+    public String getLocName() {
+        return locName;
+    }
+
+    public void setLocName(String locName) {
+        this.locName = locName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getPanAngle() {
+        return panAngle;
+    }
+
+    public void setPanAngle(double panAngle) {
+        this.panAngle = panAngle;
+    }
+
+    public double getZoom() {
+        return zoom;
+    }
+
+    public void setZoom(double zoom) {
+        this.zoom = zoom;
+    }
+    
+    
     
 }
